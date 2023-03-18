843d6bf2e459414ba4e6e48c717a59eaf2f08a5c
Mihau <michal.pryba.mail@gmail.com>
2023-03-18 18:36:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 pygame.init()
 
@@ -11,11 +12,58 @@ pygame.display.set_caption("Q*cat")
 icon = pygame.image.load('assets/icon.png') #Cat icons created by Freepik
 pygame.display.set_icon(icon)
 
+surface = pygame.Surface((736/2, 414/2)) #Create game surface with actual game resolution
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill('red')
+
+
+    a = 37
+    x = 100
+    y = 120
+    
+    pygame.draw.polygon(surface, 'gray', [(x - 0.5*a*math.sqrt(3),y+0.5*a), (x - 0.5*a*math.sqrt(3), y+1.5*a), (x , y+2*a), (x ,y+a)])
+    pygame.draw.polygon(surface, 'blue', [(x,y),(x + 0.5*a*math.sqrt(3),y+0.5*a),(x,y+a),(x - 0.5*a*math.sqrt(3),y+0.5*a)])
+    pygame.draw.polygon(surface, 'yellow', [(x + 0.5*a*math.sqrt(3),y+0.5*a), (x + 0.5*a*math.sqrt(3), y+1.5*a), (x , y+2*a), (x ,y+a)])
+
+    x = x +a*math.sqrt(3)
+
+    pygame.draw.polygon(surface, 'gray', [(x - 0.5*a*math.sqrt(3),y+0.5*a), (x - 0.5*a*math.sqrt(3), y+1.5*a), (x , y+2*a), (x ,y+a)])
+    pygame.draw.polygon(surface, 'blue', [(x,y),(x + 0.5*a*math.sqrt(3),y+0.5*a),(x,y+a),(x - 0.5*a*math.sqrt(3),y+0.5*a)])
+    pygame.draw.polygon(surface, 'yellow', [(x + 0.5*a*math.sqrt(3),y+0.5*a), (x + 0.5*a*math.sqrt(3), y+1.5*a), (x , y+2*a), (x ,y+a)])
+
+
+
+    x = x + a*math.sqrt(3)
+
+    pygame.draw.polygon(surface, 'gray', [(x - 0.5*a*math.sqrt(3),y+0.5*a), (x - 0.5*a*math.sqrt(3), y+1.5*a), (x , y+2*a), (x ,y+a)])
+    pygame.draw.polygon(surface, 'blue', [(x,y),(x + 0.5*a*math.sqrt(3),y+0.5*a),(x,y+a),(x - 0.5*a*math.sqrt(3),y+0.5*a)])
+    pygame.draw.polygon(surface, 'yellow', [(x + 0.5*a*math.sqrt(3),y+0.5*a), (x + 0.5*a*math.sqrt(3), y+1.5*a), (x , y+2*a), (x ,y+a)])
+
+    x = 100 + 0.5*a*math.sqrt(3)
+    y= y-1.5*a
+    pygame.draw.polygon(surface, 'gray', [(x - 0.5*a*math.sqrt(3),y+0.5*a), (x - 0.5*a*math.sqrt(3), y+1.5*a), (x , y+2*a), (x ,y+a)])
+    pygame.draw.polygon(surface, 'blue', [(x,y),(x + 0.5*a*math.sqrt(3),y+0.5*a),(x,y+a),(x - 0.5*a*math.sqrt(3),y+0.5*a)])
+    pygame.draw.polygon(surface, 'yellow', [(x + 0.5*a*math.sqrt(3),y+0.5*a), (x + 0.5*a*math.sqrt(3), y+1.5*a), (x , y+2*a), (x ,y+a)])
+
+    x = x + a*math.sqrt(3)
+    pygame.draw.polygon(surface, 'gray', [(x - 0.5*a*math.sqrt(3),y+0.5*a), (x - 0.5*a*math.sqrt(3), y+1.5*a), (x , y+2*a), (x ,y+a)])
+    pygame.draw.polygon(surface, 'blue', [(x,y),(x + 0.5*a*math.sqrt(3),y+0.5*a),(x,y+a),(x - 0.5*a*math.sqrt(3),y+0.5*a)])
+    pygame.draw.polygon(surface, 'yellow', [(x + 0.5*a*math.sqrt(3),y+0.5*a), (x + 0.5*a*math.sqrt(3), y+1.5*a), (x , y+2*a), (x ,y+a)])
+
+    x = 100 + a*math.sqrt(3)
+    y= y-1.5*a
+    pygame.draw.polygon(surface, 'gray', [(x - 0.5*a*math.sqrt(3),y+0.5*a), (x - 0.5*a*math.sqrt(3), y+1.5*a), (x , y+2*a), (x ,y+a)])
+    pygame.draw.polygon(surface, 'blue', [(x,y),(x + 0.5*a*math.sqrt(3),y+0.5*a),(x,y+a),(x - 0.5*a*math.sqrt(3),y+0.5*a)])
+    pygame.draw.polygon(surface, 'yellow', [(x + 0.5*a*math.sqrt(3),y+0.5*a), (x + 0.5*a*math.sqrt(3), y+1.5*a), (x , y+2*a), (x ,y+a)])
+    screen.blit(pygame.transform.scale(surface,(1600, 900)), (0,0))
+
+    #Game scale
+
+
+
     pygame.display.flip()
     clock.tick(60)
 
